98bdf4565d51942d360c23dc77f4f5d2a3bc6a13
Diego-ipt <106196745+Diego-ipt@users.noreply.github.com>
2023-10-16 20:05:31 -0300

    agregado codigo base de producto

--- src/main/java/org/example/Producto.java
+++ src/main/java/org/example/Producto.java
@@ -1,5 +1,13 @@
 package org.example;
-
-public class Producto {
-
+abstract class Producto {
+    private int serie;
+    public Producto(int serie){
+        this.serie=serie;
+    }
+    public void SetProducto(int serie){
+        this.serie=serie;
+    }
+    public int GetProducto(){
+        return serie;
+    }
 }

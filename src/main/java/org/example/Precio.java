c1c2256c97cab47b905b7c4c45b5ee7fa5e9011b
Antonia <montlopanto@gmail.com>
2023-10-17 11:58:24 -0300

    Enumeracion de precio lista

--- src/main/java/org/example/Precio.java
+++ src/main/java/org/example/Precio.java
@@ -1,5 +1,31 @@
 package org.example;
 
 enum Precio {
-    
+    COCACOLA(1, 1000), SPRITE(2,500), FANTA(3,800), SNICKERS(4,700), SUPER8(5,300);
+
+    private int precio;
+    private int opcion;
+
+    private Precio(int opcion, int precio) {
+        this.opcion = opcion;
+        this.precio = precio;
+    }
+
+    //valor modificable?
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setOpcion(int opcion) {
+        this.opcion = opcion;
+    }
+
+    public int getOpcion() {
+        return opcion;
+    }
+
 }
